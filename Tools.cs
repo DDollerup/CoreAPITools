7aba4fb5e9590ba06b43b7ba03453bbae09cfe58
Daniel Dollerup <d.dollerup@gmail.com>
2021-10-14 21:35:58 +0200

    Update Tools.cs
    Added string.IsNullOrEmpty to IsBase64String

--- Tools.cs
+++ Tools.cs
@@ -37,6 +37,7 @@ public static class Tools
 
     public static bool IsBase64String(string base64)
     {
+        if (string.IsNullOrEmpty(base64)) return false;
         Span<byte> buffer = new Span<byte>(new byte[base64.Length]);
         return Convert.TryFromBase64String(base64, buffer, out int bytesParsed);
     }
