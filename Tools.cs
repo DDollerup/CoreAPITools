c5c1728f2e662d3002895601656c3ed77ee4be65
Daniel Dollerup <d.dollerup@gmail.com>
2021-11-15 13:40:43 +0100

    Update Tools.cs
    Added functionality to allow static file serving path to be returned aswell.

--- Tools.cs
+++ Tools.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Http;
 using System;
 
 public static class Tools
@@ -35,6 +36,45 @@ public static class Tools
         return null;
     }
 
+    public static string ConvertBase64ToFile(string base64, string saveToPath, HttpContext context = null)
+    {
+        if (base64 != null)
+        {
+            var base64array = Convert.FromBase64String(base64);
+            // Assume file type
+            // jpg as default
+            string fileType = ".jpg";
+
+            switch (base64[0])
+            {
+
+                case 'i': // png
+                    fileType = ".png";
+                    break;
+                case 'R': //gif
+                    fileType = ".gif";
+                    break;
+                case 'U': // webp
+                    fileType = ".webp";
+                    break;
+                case '/': // jpeg
+                default:
+                    break;
+            }
+
+            var filePath = saveToPath + @"\" + Guid.NewGuid() + fileType;
+            System.IO.File.WriteAllBytes(filePath, base64array);
+
+            var fileName = System.IO.Path.GetFileName(filePath);
+            return context == null ? fileName : RequestServerURL(context) + fileName;
+        }
+        return null;
+    }
+
+    public static string RequestServerURL(HttpContext context)
+    {
+        return context.Request.Scheme + "://" + context.Request.Host.Value + "/";
+    }
     public static bool IsBase64String(string base64)
     {
         if (string.IsNullOrEmpty(base64)) return false;
